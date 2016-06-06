99d9cafdb534c8857e20332af04c5a1872e24414
waterwoodwind <469798015@qq.com>
2016-06-06 16:27:14 +0800

    [+]添加今日日期函数

--- arrow_time.py
+++ arrow_time.py
@@ -17,3 +17,8 @@ def arrow_influxdb_format(time_number):
     sector_number = sector_number.format('YYYY-MM-DDTHH:mm:ss')+'Z'
     sector_number = sector_number.encode('utf-8')
     return sector_number
+
+def today_date_for_influxd_sql():
+    today = arrow.utcnow().replace(hours=8)
+    today_number = today.format('YYYY-MM-DD')
+    return today_number
