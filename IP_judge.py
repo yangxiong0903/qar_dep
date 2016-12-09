8b507b9c4ea1ae50a18ae0d47081ed11bc1a6a5b
waterwoodwind <469798015@qq.com>
2016-12-09 10:56:59 +0800

    [-]暂时不处理7891

--- IP_judge.py
+++ IP_judge.py
@@ -9,7 +9,7 @@ class LOCAL():
         self.home_server_ip = "192.168.3.10"
         self.local_ip = "localhost"
     def server_ip(self):
-        if self.name == "wxjd-61222177.cq.airchina.com.cn":
+        if self.name == "WXJD-61222177.cq.airchina.com.cn":
             return self.office_server_ip
         elif self.name == "HUGE":
             return self.home_server_ip
